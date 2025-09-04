59b5c06504b969601804a1fc32d6cd6c77845573
Gracious <gpoa@bu.edu>
2025-09-04 12:03:41 -0400

    adding updated settings.py

--- cs412/settings.py
+++ cs412/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nsyby27ea6-s=mjwl%3!e%=u%_6bv7cy_3%0s&w&b5hxfma9d5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cs-webapps.bu.edu"]
 
 
 # Application definition
@@ -120,3 +120,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/' # note: no leading slash!
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL= "media/"  # note: no leading slash!
+
+import socket
+CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
+
+if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
+    STATIC_URL = '/gpoa/static/'
+    MEDIA_URL = '/gpoa/media/'
