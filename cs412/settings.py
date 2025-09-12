7df961bb06230db2b8944ff30ab46f02a18e9974
Gracious <gpoa@bu.edu>
2025-09-12 14:22:38 -0400

    editing settings.py for static files

--- cs412/settings.py
+++ cs412/settings.py
@@ -142,8 +142,8 @@ if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
     STATIC_URL = '/gpoa/static/'
     MEDIA_URL = '/gpoa/media/'
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATIC_URL = '/static/'
+    # STATICFILES_DIRS = [
+    #     os.path.join(BASE_DIR, "static"),
+    # ]
