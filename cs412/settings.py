0813f9a97c9e330cba8a1de59f99b0a22bfb44a0
Gracious <gpoa@bu.edu>
2025-09-12 13:35:49 -0400

    editing static pathname

--- cs412/settings.py
+++ cs412/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'gpoa/static/'
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/' # note: no leading slash!
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "gpoa@csa2/static"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
