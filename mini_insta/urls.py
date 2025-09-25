74c2a02a21b87045135cfd50550426f1f321dead
Gracious <gpoa@bu.edu>
2025-09-25 13:25:05 -0400

    edited base template, created show_all_profiles template and added urls

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,4 +1,9 @@
 # mini_insta/urls.py
+from django.urls import path
+from .views import ProfileListView  
 
 urlpatterns = [
+    # URL patterns for the mini_insta app
+    path('', ProfileListView.as_view(), name='show_all_profiles'),
+    path('show_all_profiles/', ProfileListView.as_view(), name='show_all_profiles'),
 ]
